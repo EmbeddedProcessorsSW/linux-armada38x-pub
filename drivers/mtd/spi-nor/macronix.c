EmbeddedProcessorsSW/linux-armada38x-pub

commit 9254cf8fa83b457f13fe1d8b2282af4980d97dfa

mtd: spi-nor: macronix: Add support for mx25um51245g
The Macronix mx25um51245g is a 1.8V, 512 Mbit device.
It support x1 and x8(8-8-8 only) modes.

Change-Id: Iba1e040d6bbd26bf666fc59b68cc356f9107754b
Signed-off-by: Witold Sadowski <wsadowski@marvell.com>
Signed-off-by: Suneel Garapati <sgarapati@marvell.com>
Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/120908
Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>

drivers/mtd/spi-nor/macronix.c
@@ -100,6 +100,9 @@ static const struct flash_info macronix_nor_parts[] = {
 	{ "mx66u2g45g",	 INFO(0xc2253c, 0, 64 * 1024, 4096)
 		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ)
 		FIXUP_FLAGS(SPI_NOR_4B_OPCODES) },
+	{ "mx25um51245g", INFO(0xc2803a, 0, 64 * 1024, 1024,
+		NO_SFDP_FLAGS(SECT_4K)
+		FIXUP_FLAGS(SPI_NOR_4B_OPCODES) },
 };
 
 static void macronix_nor_default_init(struct spi_nor *nor)

commit 09639f2c00df348a79848023dcda16dbc9c359b9

mtd: spi-nor: macronix: Fix compile error
Fixes: 9254cf8fa83b ("mtd: spi-nor: macronix: Add support for mx25um51245g")

Change-Id: Ibf145b3e5883d2c221daa7b8fa472978d523d0af
Signed-off-by: Suneel Garapati <sgarapati@marvell.com>
Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/121136
Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
Tested-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>

drivers/mtd/spi-nor/macronix.c
@@ -100,7 +100,7 @@ static const struct flash_info macronix_nor_parts[] = {
 	{ "mx66u2g45g",	 INFO(0xc2253c, 0, 64 * 1024, 4096)
 		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ)
 		FIXUP_FLAGS(SPI_NOR_4B_OPCODES) },
-	{ "mx25um51245g", INFO(0xc2803a, 0, 64 * 1024, 1024,
+	{ "mx25um51245g",  INFO(0xc2803a, 0, 64 * 1024, 1024)
 		NO_SFDP_FLAGS(SECT_4K)
 		FIXUP_FLAGS(SPI_NOR_4B_OPCODES) },
 };