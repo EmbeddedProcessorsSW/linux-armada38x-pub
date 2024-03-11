EmbeddedProcessorsSW/linux-armada38x-pub

commit 63b90fededa045d38394d2bcb654f4a6e8026967

drivers: spi: octeontx2: ACPI support for SPI driver
These changes are done to support ACPI in SPI driver for T9x Platforms.
If acpi is enabled, then the code will read the required properties from
ACPI table else from the DTS as existing.

Also support added to read the tx(rx)-bus-width from ACPI table as it is
done in DTS case. These parameters are used to set the SPI mode -
DUAL, QUAD, OCTAL.
They are getting read from the generic SPI driver but only in DTS case,
acpi case is not supported.

To overcome this, we use spi_controller_mem_ops 'supports_op' provided by
SPI driver through which we can read the bus width from APCI table and
update the spi mode before the supported bus width check in main SPI driver
is done.

Ported from 5.15 branch.

Signed-off-by: Piyush Malgujar <pmalgujar@marvell.com>
Change-Id: I4f12e259c5f1643d074297d399f49ed89f92b54a
Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/122449
Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>

drivers/spi/spi-octeontx2.c
@@ -5,11 +5,16 @@
  * Copyright (C) 2018 Marvell International Ltd.
  */
 
+#include <linux/acpi.h>
 #include <linux/of.h>
 #include <linux/delay.h>
 #include <linux/module.h>
 #include <linux/pci.h>
+#include <linux/property.h>
 #include <linux/spi/spi.h>
+#ifdef CONFIG_ACPI
+#include <linux/spi/spi-mem.h>
+#endif
 
 #include "spi-octeontx2.h"
 
@@ -231,6 +236,78 @@ err:
 	return status;
 }
 
+#ifdef CONFIG_ACPI
+static int octeontx2_spi_exec_op(struct spi_mem *mem,
+				 const struct spi_mem_op *op)
+{
+	return -EOPNOTSUPP;
+}
+
+static bool octeontx2_spi_supports_op(struct spi_mem *mem,
+				      const struct spi_mem_op *op)
+{
+	struct spi_device *spi = mem->spi;
+	const union acpi_object *obj;
+	struct acpi_device *adev;
+
+	adev = ACPI_COMPANION(&spi->dev);
+
+	if (!acpi_dev_get_property(adev, "spi-tx-bus-width", ACPI_TYPE_INTEGER,
+				   &obj)) {
+		switch (obj->integer.value) {
+		case 1:
+			break;
+		case 2:
+			spi->mode |= SPI_TX_DUAL;
+			break;
+		case 4:
+			spi->mode |= SPI_TX_QUAD;
+			break;
+		case 8:
+			spi->mode |= SPI_TX_OCTAL;
+			break;
+		default:
+			dev_warn(&spi->dev,
+				 "spi-tx-bus-width %lld not supported\n",
+				 obj->integer.value);
+			break;
+		}
+	}
+
+	if (!acpi_dev_get_property(adev, "spi-rx-bus-width", ACPI_TYPE_INTEGER,
+				   &obj)) {
+		switch (obj->integer.value) {
+		case 1:
+			break;
+		case 2:
+			spi->mode |= SPI_RX_DUAL;
+			break;
+		case 4:
+			spi->mode |= SPI_RX_QUAD;
+			break;
+		case 8:
+			spi->mode |= SPI_RX_OCTAL;
+			break;
+		default:
+			dev_warn(&spi->dev,
+				 "spi-rx-bus-width %lld not supported\n",
+				 obj->integer.value);
+			break;
+		}
+	}
+
+	if (!spi_mem_default_supports_op(mem, op))
+		return false;
+
+	return true;
+}
+
+static const struct spi_controller_mem_ops octeontx2_spi_mem_ops = {
+	.supports_op = octeontx2_spi_supports_op,
+	.exec_op = octeontx2_spi_exec_op
+};
+#endif
+
 static int octeontx2_spi_probe(struct pci_dev *pdev,
 			       const struct pci_device_id *ent)
 {
@@ -239,6 +316,7 @@ static int octeontx2_spi_probe(struct pci_dev *pdev,
 	struct octeontx2_spi *p;
 	union mpix_sts mpi_sts;
 	int ret = -ENOENT;
+	bool has_acpi;
 
 	/* may need to hunt for devtree entry */
 	if (!pdev->dev.of_node) {
@@ -251,6 +329,8 @@ static int octeontx2_spi_probe(struct pci_dev *pdev,
 		pdev->dev.of_node = np;
 		of_node_put(np);
 	}
+
+	has_acpi = has_acpi_companion(dev);
 
 	master = spi_alloc_master(dev, sizeof(struct octeontx2_spi));
 	if (!master)
@@ -282,21 +362,27 @@ static int octeontx2_spi_probe(struct pci_dev *pdev,
 	mpi_sts.u64 = readq(p->register_base + OCTEONTX2_SPI_STS(p));
 	p->rcvd_present = mpi_sts.u64 & 0x4 ? true : false;
 
-	/* FIXME: need a proper clocksource object for SCLK */
-	p->clk = devm_clk_get(dev, NULL);
-	if (IS_ERR(p->clk)) {
-		p->clk = devm_clk_get(dev, "sclk");
-		p->sys_freq = 0;
-	} else {
-		ret = clk_prepare_enable(p->clk);
-		if (!ret)
-			p->sys_freq = clk_get_rate(p->clk);
-	}
+	if (!has_acpi) {
+		/* FIXME: need a proper clocksource object for SCLK */
+		p->clk = devm_clk_get(dev, NULL);
+		if (IS_ERR(p->clk)) {
+			p->clk = devm_clk_get(dev, "sclk");
+			p->sys_freq = 0;
+		} else {
+			ret = clk_prepare_enable(p->clk);
+			if (!ret)
+				p->sys_freq = clk_get_rate(p->clk);
+		}
 
-	if (!p->sys_freq)
-		p->sys_freq = SYS_FREQ_DEFAULT;
-	if (tbi_clk_en)
-		p->sys_freq = TBI_FREQ;
+		if (!p->sys_freq)
+			p->sys_freq = SYS_FREQ_DEFAULT;
+		if (tbi_clk_en)
+			p->sys_freq = TBI_FREQ;
+	} else {
+		device_property_read_u32(dev, "sclk", &p->sys_freq);
+		if (!p->sys_freq)
+			p->sys_freq = TBI_FREQ;
+	}
 	dev_info(dev, "Reference clock is %u\n", p->sys_freq);
 
 	master->num_chipselect = 4;
@@ -308,6 +394,10 @@ static int octeontx2_spi_probe(struct pci_dev *pdev,
 	master->bits_per_word_mask = SPI_BPW_MASK(8);
 	master->max_speed_hz = OCTEONTX2_SPI_MAX_CLOCK_HZ;
 	master->dev.of_node = pdev->dev.of_node;
+	master->dev.fwnode = pdev->dev.fwnode;
+	#ifdef CONFIG_ACPI
+		master->mem_ops = &octeontx2_spi_mem_ops;
+	#endif
 
 	pci_set_drvdata(pdev, master);
 
@@ -318,7 +408,8 @@ static int octeontx2_spi_probe(struct pci_dev *pdev,
 	return 0;
 
 error_disable:
-	clk_disable_unprepare(p->clk);
+	if (!has_acpi)
+		clk_disable_unprepare(p->clk);
 error_put:
 	spi_master_put(master);
 error:
@@ -330,11 +421,14 @@ static void octeontx2_spi_remove(struct pci_dev *pdev)
 	struct spi_master *master = pci_get_drvdata(pdev);
 	struct octeontx2_spi *p;
 
+	bool has_acpi = has_acpi_companion(&pdev->dev);
+
 	p = spi_master_get_devdata(master);
 
 	/* Put everything in a known state. */
 	if (p) {
-		clk_disable_unprepare(p->clk);
+		if (!has_acpi)
+			clk_disable_unprepare(p->clk);
 		writeq(0, p->register_base + OCTEONTX2_SPI_CFG(p));
 	}
 

commit 2d3a1f3dd5971267ef6025c888831ab22517f05d

drivers: spi: octeontx2: Fix typo in return code
Change return error code in exec_op function, otherwise
all operations fail.

Fixes: 63b90fededa0 ("drivers: spi: octeontx2: ACPI support for SPI driver")

Change-Id: I9c19196c64a84c0be59724e2518ee21d1bed3e41
Signed-off-by: Suneel Garapati <sgarapati@marvell.com>
Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/123450
Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
Tested-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>

drivers/spi/spi-octeontx2.c
@@ -240,7 +240,7 @@ err:
 static int octeontx2_spi_exec_op(struct spi_mem *mem,
 				 const struct spi_mem_op *op)
 {
-	return -EOPNOTSUPP;
+	return -ENOTSUPP;
 }
 
 static bool octeontx2_spi_supports_op(struct spi_mem *mem,